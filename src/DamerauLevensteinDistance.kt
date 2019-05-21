505a30effc45bc21244c416f119f84b4e1d0469b
Stoorx <antiproton2013@yandex.ru>
2019-05-21 19:19:07 +0300

    Fix Damerau check

--- src/DamerauLevensteinDistance.kt
+++ src/DamerauLevensteinDistance.kt
@@ -19,14 +19,14 @@ object DamerauLevensteinDistance {
         if (i == 0 && j == 0) 0
         else if (j == 0) i
         else if (i == 0) j
-        else if (i > 1 && j > 1) minOf(
+        else if (i >= 2 && j >= 2) minOf(
             minOf(
                 arr[i][j - 1] + 1,
                 arr[i - 1][j] + 1
             ),
             minOf(
                 arr[i - 1][j - 1] + cost(s1[i - 1], s2[j - 1]),
-                arr[i - 2][j - 2] + 1
+                arr[i - 2][j - 2] + damerau(s1[i - 1], s2[j - 2], s1[i - 2], s2[j - 1])
             )
         )
         else minOf(
@@ -38,4 +38,8 @@ object DamerauLevensteinDistance {
     private fun cost(c1: Char, c2: Char): Int =
         if (c1 == c2) 0
         else 1
+
+    private fun damerau(c1: Char, c2: Char, c3: Char, c4: Char) =
+        if (c1 == c2 && c3 == c4) 1
+        else 1_000_000_000
 }
